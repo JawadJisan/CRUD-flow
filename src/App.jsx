79b68a17815ffae9729a5d7c95e80fb0188cce09
JawadJisan <jisan.sc@gmail.com>
2025-11-22 12:06:22 +0600

    pushing react,tailwindv4,shadcn boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p className='text-amber-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, voluptatum ad beatae consectetur soluta aperiam? Quasi quis consectetur ipsa architecto cupiditate voluptate quia. Tempora corrupti quisquam dicta possimus blanditiis ipsam.z</p>
+    </>
+  )
+}
+
+export default App
